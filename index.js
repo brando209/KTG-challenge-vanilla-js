7a5a1073a2da5926c54b202c0e7763592f09c510
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-10-26 12:09:27 -0700

    Populate user table with data complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import fetchData from "./api.js";
+import { populateUserTable } from "./users.js";
 
 async function main() {
     const [users, posts] = await fetchData();
-    console.log(users, posts);
+
+    populateUserTable(users, (id, username) => { console.log(id, username) });    
 }
 
 main();
---
+++ users.js
@@ -0,0 +1,24 @@
+const userTable = document.querySelector("table#users");
+
+export function populateUserTable(users, clickHandler = () => {}) {
+    const addUserRow = ({ id, name, username, email }) => {
+        const userRow = document.createElement("tr");
+        const userNameCell = document.createElement("td");
+        const fullNameCell = document.createElement("td");
+        const emailCell = document.createElement("td");
+        userNameCell.textContent = username;
+        fullNameCell.textContent = name;
+        emailCell.textContent = email;
+        userRow.className = "user-row";
+        userRow.id = id;
+        userRow.onclick = () => clickHandler(id, username);
+        userRow.appendChild(userNameCell);
+        userRow.appendChild(fullNameCell);
+        userRow.appendChild(emailCell);
+        userTable.querySelector("tbody").appendChild(userRow);
+    }
+
+    for(let user of users) {
+        addUserRow(user);
+    }
+}
